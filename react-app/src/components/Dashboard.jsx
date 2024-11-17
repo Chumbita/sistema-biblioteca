a91e5254dde053ca8aaa6047b5b5a6bd71608639
Chumbita <chumbitalexis@gmail.com>
2024-11-17 16:00:42 -0300

    Se agregó una imágen y modifcación del dashboard

--- react-app/src/components/Dashboard.jsx
+++ react-app/src/components/Dashboard.jsx
@@ -19,7 +19,7 @@ const user = {
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 const navigation = [
-  { name: "Libros", href: "/dashboard/libros", current: true },
+  { name: "Libros", href: "/dashboard/libros", current: false },
   { name: "Secciones", href: "/dashboard/secciones", current: false },
   { name: "Miembros", href: "/dashboard/miembros", current: false },
   { name: "Prestamos", href: "/dashboard/prestamos", current: false },
@@ -34,7 +34,7 @@ function classNames(...classes) {
 
 const Dashboard = () => {
   const navigate = useNavigate();
-  const { logout } = useContext(AuthContext); // Obtener el token
+  const { logout } = useContext(AuthContext);
 
   return (
     <>
