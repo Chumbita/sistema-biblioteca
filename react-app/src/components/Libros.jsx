65e356aca7709583baed66cd7c9831e752d6648b
Carlos Olivera <carlitosolivera1510@gmail.com>
2024-11-17 20:40:12 -0300

    Se agregó las funcionalidades de eliminar y editar en la sección de libros

--- react-app/src/components/Libros.jsx
+++ react-app/src/components/Libros.jsx
@@ -1,11 +1,28 @@
 import React, { useEffect, useState, useContext } from "react";
 import { AuthContext } from "../context/AuthProvider";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
+import Modal from "./Modal";
 
 const Libros = () => {
+  const { token } = useContext(AuthContext);
   const [books, setBooks] = useState([]);
   const [selectedBook, setSelectedBook] = useState(null);
-  const { token } = useContext(AuthContext);
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+  const [formData, setFormData] = useState({});
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const inputs = [
+    { name: "titulo", label: "Titulo", type: "text" },
+    { name: "autor", label: "Autor", type: "text" },
+    { name: "genero", label: "Genero", type: "text" },
+    { name: "ISBN", label: "ISBN", type: "text" },
+    { name: "numero_estante", label: "Numero de estante", type: "number" },
+    { name: "numero_repisa", label: "Numero de repisa", type: "number" },
+    { name: "estado", label: "Disponibilidad", type: "text" },
+  ];
 
   const getBooks = async () => {
     if (!token) {
@@ -34,9 +51,119 @@ const Libros = () => {
     }
   }, [token]);
 
+  const confirmDelete = (id) => {
+    confirmAlert({
+      title: "Confirmar eliminación",
+      message: "¿Estás seguro de que deseas eliminar este libro?",
+      buttons: [
+        {
+          label: "Sí",
+          onClick: () => {
+            removeBook(id).then(() => {
+              setSelectedBook(null);
+            });
+          },
+        },
+        {
+          label: "No",
+          onClick: () => {},
+        },
+      ],
+    });
+  };
+
+  const removeBook = async (id) => {
+    if (!token) {
+      return;
+    }
+
+    try {
+      const response = await fetch(`http://localhost:3000/libros/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setBooks((prevBooks) => prevBooks.filter((book) => book.id !== id));
+        setMessage(data.message);
+      } else {
+        setError(data.message);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const handleOpenModal = () => setModalOpen(true);
+  const handleCloseModal = () => setModalOpen(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleEditClick = (book) => {
+    setSelectedBook(book);
+    setFormData(book);
+    setModalOpen(true);
+  };
+
+  const handleEdit = async () => {
+    if (!token) return;
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/libros/${selectedBook.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setBooks((prevBooks) =>
+          prevBooks.map((book) =>
+            book.id === selectedBook.id ? { ...book, ...formData } : book
+          )
+        );
+
+        setMessage("Libro actualizado exitosamente");
+      } else {
+        setError(data.message);
+      }
+
+      handleCloseModal();
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="bg-white">
       <h1 className="text-2xl font-bold text-gray-900">Libros</h1>
+      <span className="sm:ml-3 ">
+        <button
+          type="button"
+          // onClick={handleOpenModal}
+          onClick={() =>
+            window.open("https://www.instagram.com/chumbitaluciano/", "_blank")
+          }
+          className="inline-flex mt-4 items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Agregar
+        </button>
+      </span>
       <div className=" max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {books.map((book) => (
@@ -70,6 +197,15 @@ const Libros = () => {
       {selectedBook && (
         <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white p-6 rounded-lg max-w-md w-full">
+            <Modal
+              title="Editar miembro"
+              isOpen={isModalOpen}
+              inputs={inputs}
+              formData={formData}
+              onClose={handleCloseModal}
+              onSubmit={handleEdit}
+              handleChange={handleChange}
+            />
             <h2 className="text-2xl font-bold text-gray-900">
               {selectedBook.titulo}
             </h2>
@@ -95,12 +231,25 @@ const Libros = () => {
               <strong>Disponibilidad:</strong>{" "}
               {selectedBook.estado ? "Disponible" : "No disponible"}
             </p>
-            <div className="flex justify-end mt-4">
+            <div className="d-flex">
               <button
-                className="px-4 py-2 bg-gray-200 rounded-lg"
+                className="px-4 py-2 bg-gray-200 rounded-lg me-3"
                 onClick={() => setSelectedBook(null)}
               >
                 Cerrar
+              </button>
+
+              <button
+                className="px-4 py-2 bg-gray-200 rounded-lg me-3"
+                onClick={() => handleEditClick(selectedBook)}
+              >
+                <i className="edit-icon bi bi-pencil-square"></i> Editar
+              </button>
+              <button
+                className="btn btn-danger"
+                onClick={() => confirmDelete(selectedBook.id)}
+              >
+                <i className="bi bi-trash me-2"></i> Eliminar
               </button>
             </div>
           </div>
